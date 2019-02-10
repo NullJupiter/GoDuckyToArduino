8c540e9a6c5231e175385d708040a02053d40010
NullJupiter <markhaube@gmx.de>
2019-02-10 02:37:27 +0100

    Added rough infrastructure of project.

--- src/main.go
+++ src/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
+func main() {
+	// Check if file path is given
+	if len(os.Args) != 2 {
+		fmt.Println("You need to give the program a file.")
+		fmt.Println("Usage:", os.Args[0], "[FILE_PATH]")
+		os.Exit(1)
+	}
+
+	// Read the provided file
+	duckyCodeBytes, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println("Could not find the provided file in your filesystem or there was an error reading from it.")
+		os.Exit(1)
+	}
+	duckyCode := string(duckyCodeBytes)
+
+	// Look through every line in seperate and convert it
+	for _, line := range strings.Split(duckyCode, "\n") {
+		fmt.Println(convert(line))
+	}
+}
+
+// Function to convert the given line
+func convert(line string) (string, error) {
+	return line, nil
 }
